6c79bec72704c379fce26509a29f79c7021bf211
Kilyong Jeong <uzoolove@gmail.com>
2024-03-19 21:26:26 +0900

    🔧

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static('../public'));
+app.use(express.static('./public'));
 
 // app.use('/', (req, res, next) => {
 //   setTimeout(next, 1000);
